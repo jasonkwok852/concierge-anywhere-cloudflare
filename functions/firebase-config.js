a741d53750346278b1795811151c4b6928383176
jasonkwok852 <smit66476835@gmail.com>
2025-06-17 19:17:02 +0800

    Update firebase-config.js

--- functions/firebase-config.js
+++ functions/firebase-config.js
@@ -1,18 +1,19 @@
-export async function onRequest(context) {
-    const firebaseConfig = {
+// 檔案: /functions/firebase-config.js
+export async function onRequestGet(context) {
+    // 從環境變量獲取配置
+    const config = {
         apiKey: context.env.FIREBASE_WEB_API_KEY,
         authDomain: "cloudflare-concierge-anywhere.firebaseapp.com",
         projectId: "cloudflare-concierge-anywhere",
         storageBucket: "cloudflare-concierge-anywhere.appspot.com",
         messagingSenderId: "2628861039",
-        appId: "1:2628861039:web:bf1873052440751d25219a",
-        measurementId: "G-1NDH2C02FF"
+        appId: "1:2628861039:web:bf1873052440751d25219a"
     };
-    
-    return new Response(JSON.stringify(firebaseConfig), {
-        headers: { 
+
+    return new Response(JSON.stringify(config), {
+        headers: {
             'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': 'https://concierge-anywhere.com'
+            'Access-Control-Allow-Origin': '*'
         }
     });
 }
